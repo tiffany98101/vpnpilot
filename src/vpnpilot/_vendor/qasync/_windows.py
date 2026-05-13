c01fb9066ab6813d8f5fa81cc3478c99b7f1af36
Tiffany von Arnim <tiffany.vonarnim@gmail.com>
2026-05-13 16:52:37 -0700

    Untrack .rpmbuild/ and restore vendored qasync to upstream

--- src/vpnpilot/_vendor/qasync/_windows.py
+++ src/vpnpilot/_vendor/qasync/_windows.py
@@ -63,7 +63,7 @@ class _ProactorEventLoop(asyncio.ProactorEventLoop):
 class _IocpProactor(windows_events.IocpProactor):
     def __init__(self):
         self.__events = []
-        super().__init__()
+        super(_IocpProactor, self).__init__()
         self._lock = threading.Lock()
 
     def select(self, timeout=None):
@@ -76,57 +76,57 @@ class _IocpProactor(windows_events.IocpProactor):
 
     def close(self):
         self._logger.debug("Closing")
-        super().close()
+        super(_IocpProactor, self).close()
 
     # Wrap all I/O submission methods to acquire the internal lock first; listed
     # in the order they appear in the base class source code.
 
     def recv(self, conn, nbytes, flags=0):
         with self._lock:
-            return super().recv(conn, nbytes, flags)
+            return super(_IocpProactor, self).recv(conn, nbytes, flags)
 
     def recv_into(self, conn, buf, flags=0):
         with self._lock:
-            return super().recv_into(conn, buf, flags)
+            return super(_IocpProactor, self).recv_into(conn, buf, flags)
 
     def recvfrom(self, conn, nbytes, flags=0):
         with self._lock:
-            return super().recvfrom(conn, nbytes, flags)
+            return super(_IocpProactor, self).recvfrom(conn, nbytes, flags)
 
     def recvfrom_into(self, conn, buf, flags=0):
         with self._lock:
-            return super().recvfrom_into(conn, buf, flags)
+            return super(_IocpProactor, self).recvfrom_into(conn, buf, flags)
 
     def sendto(self, conn, buf, flags=0, addr=None):
         with self._lock:
-            return super().sendto(conn, buf, flags, addr)
+            return super(_IocpProactor, self).sendto(conn, buf, flags, addr)
 
     def send(self, conn, buf, flags=0):
         with self._lock:
-            return super().send(conn, buf, flags)
+            return super(_IocpProactor, self).send(conn, buf, flags)
 
     def accept(self, listener):
         with self._lock:
-            return super().accept(listener)
+            return super(_IocpProactor, self).accept(listener)
 
     def connect(self, conn, address):
         with self._lock:
-            return super().connect(conn, address)
+            return super(_IocpProactor, self).connect(conn, address)
 
     def sendfile(self, sock, file, offset, count):
         with self._lock:
-            return super().sendfile(sock, file, offset, count)
+            return super(_IocpProactor, self).sendfile(sock, file, offset, count)
 
     def accept_pipe(self, pipe):
         with self._lock:
-            return super().accept_pipe(pipe)
+            return super(_IocpProactor, self).accept_pipe(pipe)
 
     # connect_pipe() does not actually use the delayed completion machinery.
 
     # This takes care of wait_for_handle() too.
     def _wait_for_handle(self, handle, timeout, _is_cancel):
         with self._lock:
-            return super()._wait_for_handle(
+            return super(_IocpProactor, self)._wait_for_handle(
                 handle, timeout, _is_cancel
             )
 
